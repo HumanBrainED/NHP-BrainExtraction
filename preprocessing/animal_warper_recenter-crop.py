6820f691957a69f914ead750422d001f7d20e6d3
Xinhui Li <sucyli1017@gmail.com>
2021-04-02 20:54:15 -0400

    update scripts

--- preprocessing/animal_warper_recenter-crop.py
+++ preprocessing/animal_warper_recenter-crop.py
@@ -1,45 +1,85 @@
 import os
 import sys
+import getopt
+import argparse
 import numpy as np
 import nibabel as nb
 
-# TODO XL test code
+def run_animal_warper(wd, t1, mask, template, template_mask):
 
-# TODO change paths here
-wd = '' # working directory
-t1 = '' # T1w path
-mask = '' # initial mask path, it can be U-Net or manual mask
-base="NMT_v2.0_sym/NMT_v2.0_sym_05mm/NMT_v2.0_sym_05mm.nii.gz" # NMT template
-nmtmask="NMT_v2.0_sym/NMT_v2.0_sym_05mm/NMT_v2.0_sym_05mm_brainmask.nii.gz" # NMT brain mask
+    os.chdir(wd)
 
-os.chdir(wd)
+    # load T1w and mask data
+    t1_data = nb.load(t1).get_fdata()
+    mask_data = nb.load(mask).get_fdata()
 
-# load T1w and mask data
-t1_data = nb.load(t1).get_fdata()
-mask_data = nb.load(mask).get_fdata()
+    # calculate new center position
+    nonzero_index = np.unique(np.nonzero(mask_data)[2])
+    nonzero_bottom_index = nonzero_index[0]
+    nonzero_top_index = nonzero_index[-1]
+    nonzero_center = (nonzero_bottom_index + nonzero_top_index) / 2
+    img_length = mask_data.shape[2]
+    top_diff = img_length - nonzero_top_index
+    img_center = img_length / 2
+    center_diff = nonzero_center - img_center
 
-# calculate new center position
-nonzero_index = np.unique(np.nonzero(mask_data)[2])
-nonzero_bottom_index = nonzero_index[0]
-nonzero_top_index = nonzero_index[-1]
-nonzero_center = (nonzero_bottom_index + nonzero_top_index) / 2
-img_length = mask_data.shape[2]
-top_diff = img_length - nonzero_top_index
-img_center = img_length / 2
-center_diff = nonzero_center - img_center
+    # crop T1w based on new center position
+    t1_data_new = t1_data[:, :, center_diff*2:]
+    t1_data_new[:, :, 0:top_diff] = 0
+    data_zero = np.zeros((t1_data.shape[0], t1_data.shape[1], center_diff))
+    data = np.concatenate((data_zero, t1_data_new, data_zero), axis=2)
+    out = nb.Nifti1Image(data, affine=nb.load(t1).affine)
 
-# crop T1w based on new center position
-t1_data_new = t1_data[:, :, center_diff*2:]
-t1_data_new[:, :, 0:top_diff] = 0
-data_zero = np.zeros((t1_data.shape[0], t1_data.shape[1], center_diff))
-data = np.concatenate((data_zero, t1_data_new, data_zero), axis=2)
-out = nb.Nifti1Image(data, affine=nb.load(t1).affine)
+    # TODO change recentered+cropped T1w filename if necessary
+    if '/' in t1:
+        t1_new = t1[t1.rindex('/')+1:t1.rindex('.nii.gz')] + '_shift.nii.gz'
+    else:
+        t1_new = t1[0:t1.rindex('.nii.gz')] + '_shift.nii.gz'
+    out.to_filename(t1_new)
 
-# TODO change recentered+cropped T1w filename if necessary
-t1_new = t1[t1.rindex('/')+1:t1.rindex('.nii.gz')] + '_shift.nii.gz'
-out.to_filename(t1_new)
+    # run @animal_warper with recentered+cropped T1w
+    # it will create a folder called aw_results in working directory
+    cmd="@animal_warper -input %s -base %s -skullstrip %s" % (t1_new, template, template_mask)
+    os.system(cmd)
 
-# run @animal_warper with recentered+cropped T1w
-# it will create a folder called aw_results in working directory
-cmd="@animal_warper -input %s -base %s -skullstrip %s"%(t1_new, base, nmtmask)
-os.system(cmd)
+
+if __name__=='__main__':
+
+    ## Running instructions ##
+
+    # For example,
+    # if all data is in the same folder as following:
+    # /home/xli/data/site-princeton/sub-032114
+    #  - sub-032114_ses-001_run-1_T1w.nii.gz
+    #  - sub-032114_ses-001_run-1_T1w_mask.nii.gz
+    #  - NMT_v2.0_sym/NMT_v2.0_sym_05mm/NMT_v2.0_sym_05mm.nii.gz
+    #  - NMT_v2.0_sym/NMT_v2.0_sym_05mm/NMT_v2.0_sym_05mm_brainmask.nii.gz
+
+    # we can use this command:
+    # python animal_warper_recenter-crop.py \
+    # -w /home/xli/data/sub-032114 \
+    # -t sub-032114_ses-001_run-1_T1w.nii.gz \
+    # -m sub-032114_ses-001_run-1_T1w_mask.nii.gz \
+    # -tt NMT_v2.0_sym/NMT_v2.0_sym_05mm/NMT_v2.0_sym_05mm.nii.gz \
+    # -tm NMT_v2.0_sym/NMT_v2.0_sym_05mm/NMT_v2.0_sym_05mm_brainmask.nii.gz
+
+    # full paths also work
+
+    # arguments
+    parser = argparse.ArgumentParser(description='Training Model', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    optional=parser._action_groups.pop()
+    required=parser.add_argument_group('required arguments')
+
+    # required options
+    required.add_argument('-w', '--working_dir', type=str, required=True, help='working directory')
+    required.add_argument('-t', '--t1_path', type=str, required=True, help='t1w path')
+    required.add_argument('-m', '--mask_path', type=str, required=True, help='mask path')
+    required.add_argument('-tt', '--template_t1_path', type=str, required=True, help='template t1 path')
+    required.add_argument('-tm', '--template_mask_path', type=str, required=True, help='template mask path')
+
+    if len(sys.argv)==1:
+        parser.print_help()
+        sys.exit(1)
+    args = parser.parse_args()
+
+    run_animal_warper(args.working_dir, args.t1_path, args.mask_path, args.template_t1_path, args.template_mask_path)
---
+++ preprocessing/ants_afni_fs_fsl_fs+_fsl+.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+# Input example: 
+# /home/xli/data/site-newcastle/sub-032104/sub-032104_ses-003_run-1_T1w.nii.gz
+
+# Core parameter for ANTs
+export ITK_GLOBAL_DEFAULT_NUMBER_OF_THREADS=1
+i=$1
+site=`echo ${i} | cut -d '/' -f8`
+sub=`echo ${i} | cut -d '/' -f9`
+file=`basename ${i}`
+filestring=`echo ${file} | cut -d '.' -f1`
+
+outdir=/output
+scriptsdir=/scripts
+tmp_head=NMT_0.5mm/tmp_head/NMT_0.5mm
+tmp_brain=NMT_0.5mm/tmp_brain/NMT_SS_0.5mm
+
+# Run AFNI
+# mkdir -p ${outdir}/afni/${site}/${sub}
+OMP_NUMTHREADS=1 3dSkullStrip -input ${i} -prefix ${outdir}/afni/${site}/${sub}/${filestring}_brainmask_afni.nii.gz -push_to_edge -monkey -shrink_fac 0.5
+fslmaths ${outdir}/afni/${site}/${sub}/${filestring}_brainmask_afni.nii.gz -bin ${outdir}/afni/${site}/${sub}/${filestring}_brainmask_afni_mask.nii.gz
+
+# Run FS
+# mkdir -p ${outdir}/fs/${site}/${sub}
+OMP_NUMTHREADS=1 mri_watershed -r 60 -T1 ${i} ${outdir}/fs/${site}/${sub}/${filestring}_brainmask_fs.nii.gz
+OMP_NUMTHREADS=1 fslmaths ${outdir}/fs/${site}/${sub}/${filestring}_brainmask_fs.nii.gz -bin ${outdir}/fs/${site}/${sub}/${filestring}_brainmask_fs_mask.nii.gz
+echo ${outdir}/fs/${site}/${sub}/${filestring}_brainmask_fs_mask.nii.gz
+
+# Run FSL
+# mkdir -p ${outdir}/fsl/${site}/${sub}
+OMP_NUMTHREADS=1 bet ${i} ${outdir}/fsl/${site}/${sub}/${filestring}_fsl.nii.gz -v -f 0.3 -g 0.0 -r 60 -m
+
+# Run ANTS for monkey data using NMT template:
+# mkdir -p ${outdir}/ants/${site}/${sub}
+cp ${i} ${outdir}/ants/${site}/${sub}/
+imgdir=${outdir}/ants/${site}/${sub}/
+img=${filestring}
+
+OMP_NUMTHREADS=1 bash ${scriptsdir}/MaskandRegister.bash ${imgdir} ${img} ${tmp_head} ${tmp_brain}
+rm ${outdir}/ants/${site}/${sub}/${filestring}.nii.gz
+
+# Optimal options from Zhao (2018), Bayesian Convolutional Neural Network Based MRI Brain Extraction on Nonhuman Primates:
+# AFNI: -push_to_edge -monkey -shrink_fac 0.5
+# HWA (mri_watershed): -atlas *** Also adding BRAIN radius ("-r")*** 
+# BET: -f 0.3 -g -0.5 -r 35
+
+# Run FS+
+# mkdir -p ${outdir}/fs_zhao-options/${site}/${sub}
+
+# Get brain radius (30/voxel size):
+voxSize=`3dinfo -ad3 ${i} | sort -n | awk '{print $3}'` # Get largest voxel dimension
+brain_rad=`echo "${voxSize}"|  awk '{print 30/$1}'` # Divide by 30 to get mm
+
+OMP_NUMTHREADS=1 mri_watershed -atlas -T1 -r ${brain_rad} ${i} ${outdir}/fs_zhao-options/${site}/${sub}/${filestring}_brainmask_fs.nii.gz 
+OMP_NUMTHREADS=1 fslmaths ${outdir}/fs_zhao-options/${site}/${sub}/${filestring}_brainmask_fs.nii.gz -bin ${outdir}/fs_zhao-options/${site}/${sub}/${filestring}_brainmask_fs_mask.nii.gz
+
+# Run FSL+
+# mkdir -p ${outdir}/fsl_zhao-options/${site}/${sub}
+OMP_NUMTHREADS=1 bet ${i} ${outdir}/fsl_zhao-options/${site}/${sub}/${filestring}_fsl.nii.gz -f 0.3 -g -0.5 -r 35 -m # "-m": output mask
