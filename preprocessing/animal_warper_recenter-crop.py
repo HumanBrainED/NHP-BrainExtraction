ffdb59555ba670e0f2408a2ce307ed6ecf76162a
Xinhui Li <sucyli1017@gmail.com>
2021-04-13 09:37:44 -0400

    remove @animal_warper command

--- preprocessing/animal_warper_recenter-crop.py
+++ preprocessing/animal_warper_recenter-crop.py
@@ -1,4 +1,24 @@
-'''Script to recenter and crop T1w data and run AFNI @animal_warper on new T1w
+'''Script to recenter and crop T1w data for AFNI @animal_warper
+
+Instructions
+
+For example, if all data (T1w, initial mask, NMT T1w, NMT mask) 
+is in the same folder as following:
+
+/home/xli/data/site-princeton/sub-032114
+    - sub-032114_ses-001_run-1_T1w.nii.gz
+    - sub-032114_ses-001_run-1_T1w_mask.nii.gz
+    - NMT_v2.0_sym/NMT_v2.0_sym_05mm/NMT_v2.0_sym_05mm.nii.gz
+    - NMT_v2.0_sym/NMT_v2.0_sym_05mm/NMT_v2.0_sym_05mm_brainmask.nii.gz
+
+We can offer relative or absolute paths to run the script:
+
+python animal_warper_recenter-crop.py \
+-w /home/xli/data/sub-032114 \
+-t sub-032114_ses-001_run-1_T1w.nii.gz \
+-m sub-032114_ses-001_run-1_T1w_mask.nii.gz \
+-tt NMT_v2.0_sym/NMT_v2.0_sym_05mm/NMT_v2.0_sym_05mm.nii.gz \
+-tm NMT_v2.0_sym/NMT_v2.0_sym_05mm/NMT_v2.0_sym_05mm_brainmask.nii.gz
 
 author: Xinhui Li 04/02/21
 '''
@@ -10,7 +30,7 @@ import argparse
 import numpy as np
 import nibabel as nb
 
-def run_animal_warper(wd, t1, mask, template, template_mask):
+def recenter_crop_t1(wd, t1, mask, template, template_mask):
 
     os.chdir(wd)
 
@@ -35,40 +55,15 @@ def run_animal_warper(wd, t1, mask, template, template_mask):
     data = np.concatenate((data_zero, t1_data_new, data_zero), axis=2)
     out = nb.Nifti1Image(data, affine=nb.load(t1).affine)
 
-    # TODO change recentered+cropped T1w filename if necessary
+    # change recentered+cropped T1w filename if necessary
     if '/' in t1:
         t1_new = t1[t1.rindex('/')+1:t1.rindex('.nii.gz')] + '_shift.nii.gz'
     else:
         t1_new = t1[0:t1.rindex('.nii.gz')] + '_shift.nii.gz'
     out.to_filename(t1_new)
 
-    # run @animal_warper with recentered+cropped T1w
-    # it will create a folder called aw_results in working directory
-    cmd="@animal_warper -input %s -base %s -skullstrip %s" % (t1_new, template, template_mask)
-    os.system(cmd)
-
 
 if __name__=='__main__':
-
-    ## Running instructions ##
-
-    # For example,
-    # if all data is in the same folder as following:
-    # /home/xli/data/site-princeton/sub-032114
-    #  - sub-032114_ses-001_run-1_T1w.nii.gz
-    #  - sub-032114_ses-001_run-1_T1w_mask.nii.gz
-    #  - NMT_v2.0_sym/NMT_v2.0_sym_05mm/NMT_v2.0_sym_05mm.nii.gz
-    #  - NMT_v2.0_sym/NMT_v2.0_sym_05mm/NMT_v2.0_sym_05mm_brainmask.nii.gz
-
-    # we can use this command:
-    # python animal_warper_recenter-crop.py \
-    # -w /home/xli/data/sub-032114 \
-    # -t sub-032114_ses-001_run-1_T1w.nii.gz \
-    # -m sub-032114_ses-001_run-1_T1w_mask.nii.gz \
-    # -tt NMT_v2.0_sym/NMT_v2.0_sym_05mm/NMT_v2.0_sym_05mm.nii.gz \
-    # -tm NMT_v2.0_sym/NMT_v2.0_sym_05mm/NMT_v2.0_sym_05mm_brainmask.nii.gz
-
-    # full paths also work
 
     # arguments
     parser = argparse.ArgumentParser(description='Training Model', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
@@ -87,4 +82,4 @@ if __name__=='__main__':
         sys.exit(1)
     args = parser.parse_args()
 
-    run_animal_warper(args.working_dir, args.t1_path, args.mask_path, args.template_t1_path, args.template_mask_path)
+    recenter_crop_t1(args.working_dir, args.t1_path, args.mask_path, args.template_t1_path, args.template_mask_path)
